2d94d9eaca4e859872298ce9a714a1d68f565c6d
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 12:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make,model){
+		this.make=make;
+		this.model=model;
+	}
+	get getMakeModel(){
+		return (this.make+" "+this.model);
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) extends Car {
+	constructor(make,model,topSpeed)
+	{
+		super(make,model);
+		this.topSpeed=topSpeed;
+	}
+	get getTopSpeed(){
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
